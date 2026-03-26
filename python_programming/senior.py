75bbf00c3eba3ec15e75f3705f9358ed797ef804
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-03-26 21:13:50 +0530

    Add a program to identify senior citizen

---
+++ python_programming/senior.py
@@ -0,0 +1,8 @@
+name=input("Enter your name: ")
+pre=2026
+age=int(input("Enter your birth year: "))
+age=pre-age
+if(age>18):
+    print("You are a senior citizen! ")
+else: 
+    print("You are not a senior citizen!")
