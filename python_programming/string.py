1cb600863156b3493f280107f0bd613a0c6904f9
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-03-30 11:24:04 +0530

    add folders

--- python_programming/string.py
+++ python_programming/string.py
@@ -1,13 +1,16 @@
-with open("C:\\Users\\Gangadhar\\OneDrive\\03_Projects\\14_practice_playground\\basics-for-practice\\python_programming\\sample.txt", "r") as infile:
-    lines = infile.readlines()
+# with open("C:\\Users\\Gangadhar\\OneDrive\\03_Projects\\14_practice_playground\\basics-for-practice\\python_programming\\sample.txt", "r") as infile:
+#     lines = infile.readlines()
 
-cleaned_lines = []
-for line in lines:
-    cleaned_lines.append(line.strip())
-# cleaned_lines.sort()
+# cleaned_lines = []
+# for line in lines:
+#     cleaned_lines.append(line.strip())
+# # cleaned_lines.sort()
 
-with open("output.txt", "a") as outfile:
-    for line in cleaned_lines:
-        outfile.write(line + "\n")
-
-print("Sorting complete! Check output.txt for results.")
+# with open("output.txt", "a") as outfile:
+#     for line in cleaned_lines:
+#         outfile.write(line + "\n")
+x=[1,2,1,0,4,5]
+y=x.copy()
+x.sort()
+print(x)
+print(y)
---
+++ python_programming/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+x=34
+def fun():
+    x=56
+    print(x)
+fun()
+print("python is "+x)
