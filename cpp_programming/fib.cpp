2d5b52195ad0d6b18a8f94e7bd91be4eebc4b2d7
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-03-30 12:51:51 +0530

    Add fib program in cpp

---
+++ cpp_programming/fib.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int fib();
+    cout<<"Enter the digit: "<<endl;
+    cin>>n;
+    int res=fib();
+    cout<<res;
+    return 0;
+}
+int fib(){
+    return 3;
+}
